107c985db5a4f1ec43c4ca8b8b1d79fbc61dae73
André Vinícius <167132765+branchescunha@users.noreply.github.com>
2026-01-21 17:02:13 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto com fins educativos
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Teste decoreba</h1>
+  return <h1>Teste Decoreba</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function testeerro() {
+  console.log("testeerro");
 }
 
 export default Home;
