7d05747f6acf10086b8734c31d813cad3d5ab376
André Vinícius <167132765+branchescunha@users.noreply.github.com>
2026-01-24 22:09:20 +0000

    atualizando frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Meu primeiro site no ar</h1>;
+  return (
+    <h1>
+      Eu namoro a menina mais bonita do mundo todo<br>Eu te amo meu amor 🥰</br>
+    </h1>
+  );
 }
 
 export default Home;
