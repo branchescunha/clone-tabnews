18b7b043456e6d6d4235a782644b9c8dc9da37af
André Vinícius <167132765+branchescunha@users.noreply.github.com>
2026-01-24 22:05:49 +0000

    atualizando

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,11 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sun;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "God's in charge" });
+  const updatedAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+  });
 }
 
 export default status;
