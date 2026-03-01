4900b289462292755afabf52b825948532f80830
André Vinícius <167132765+branchescunha@users.noreply.github.com>
2026-03-01 13:37:27 +0000

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,6 +1,5 @@
 import { version as uuidVersion } from "uuid";
 import orchestrator from "tests/orchestrator";
-import status from "pages/api/v1/status";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
